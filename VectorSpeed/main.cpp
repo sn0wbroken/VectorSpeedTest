b871da36045afe99c68d2fb3e6705a1791b52731
Chief(Okada) <ghjk1@live.jp>
2018-12-10 12:43:13 +0900

    add comment

--- VectorSpeed/main.cpp
+++ VectorSpeed/main.cpp
@@ -9,13 +9,13 @@ auto main()->int
 	std::vector<int> b;
 	std::vector<int> c;
 	std::chrono::system_clock::time_point  start, end; // 型は auto で可
-	double elapsed;
+	double elapsed; //経過時間
 
 	start = std::chrono::system_clock::now(); // 計測開始時間
 	a.assign(10000000, 1);
 	for (int i = 0; i < 1000000;i++)
 	{
-		a[i] = 0;
+		a[i] = 0; //代入なのは許してほしい これしかないと思った
 	}
 	a.shrink_to_fit();
 	end = std::chrono::system_clock::now();  // 計測終了時間
@@ -44,6 +44,7 @@ auto main()->int
 	elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count(); //処理に要した時間をミリ秒に変換
 	std::cout << "resize:" << elapsed << std::endl;
 
+	//結果表示をするため
 	Sleep(5000);
 	return 0;
 }
